942c808a4f7dfc4ec809f2bde400e712f05526c6
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-05-09 14:04:01 +0700

    menambahkan class prioritas

---
+++ ProgramMy-Task/Prioritas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramMy_Task
+{
+    internal class Prioritas
+    {
+    }
+}
