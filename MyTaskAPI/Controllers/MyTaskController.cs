bb889357d6600829516bbd69c74734216b96ce66
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-05-15 06:13:20 +0700

    Syahreza-Menambahkan post account dan task pada API, Menambahkan put nama dan password pada account

--- MyTaskAPI/Controllers/MyTaskController.cs
+++ MyTaskAPI/Controllers/MyTaskController.cs
@@ -11,6 +11,50 @@ namespace MyTaskAPI.Controllers
         private static readonly List<AuthenticationLibrary.Account> listAccount = new List<AuthenticationLibrary.Account>();
         private static readonly List<CRUDTaskLibrary.Task> listTask = new List<CRUDTaskLibrary.Task>();
 
+        [HttpPost(Name = "Create/Add Account")]
+        public IActionResult Create(AuthenticationLibrary.Account account)
+        {
+            listAccount.Add(account);
+            return Ok();
+        }
+
+        [HttpPost(Name = "Create/Add Task")]
+        public IActionResult Create(CRUDTaskLibrary.Task task)
+        {
+            listTask.Add(task);
+            return Ok();
+        }
+
+        [HttpPut(Name = "Update Account Name")]
+        public IActionResult Update(string username, string newName)
+        {
+            foreach (var account in listAccount)
+            {
+                if (account.userName == username)
+                {
+                    account.nama = newName;
+                    return Ok();
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpPut(Name = "Update Account Password")]
+        public IActionResult Update(string username, string newPassword)
+        {
+            foreach (var account in listAccount)
+            {
+                if (account.userName == username)
+                {
+                    account.password = newPassword;
+                    return Ok();
+                }
+            }
+            return NotFound();
+        }
+
+        private readonly ILogger<MyTaskController> _logger;
+
         public MyTaskController(ILogger<MyTaskController> logger)
         {
             _logger = logger;
