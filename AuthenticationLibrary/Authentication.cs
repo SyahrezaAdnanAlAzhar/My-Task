52f167a7a73dd317d7fd9a5422f9cae850420e04
AhmadFadliAkbar <135632054+AhmadFadliAkbar@users.noreply.github.com>
2024-05-15 13:56:32 +0700

    json - Update Authentication.cs

--- AuthenticationLibrary/Authentication.cs
+++ AuthenticationLibrary/Authentication.cs
@@ -63,4 +63,6 @@ namespace AuthenticationLibrary
             return !accounts.Any(a => a.userName == username);
         }
     }
+    String Authentication = "Users/ahmadfadliakbar/Projects/My-Task/AuthenticationLibrary/Authentication.json";
+    Authentication auth = new Authentication(Authentication.json);
 }
